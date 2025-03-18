19c63429aa79b88a5fbf606e90497c0e9ff72bed
Rajan Rajbhar <rajannrajb502@gmail.com>
2025-03-18 14:40:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Menu toggle functionality
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      900: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1500: {
+        slidesPerView: 5,
+        spaceBetween: 25,
+      },
+    },
+  });
+  
